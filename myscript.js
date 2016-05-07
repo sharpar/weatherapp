373a17c83086796c07b4aece157e0c817c2ba91b
sharpar <shivain27@gmail.com>
2016-05-06 23:44:55 -0400

    working with weather api

--- myscript.js
+++ myscript.js
@@ -3,30 +3,28 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-var x = document.getElementById("demo");
-
-function showPosition(position) {
-    'use strict';
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-}
-
 function getLocation() {
     'use strict';
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+    $.get("http://ipinfo.io", function (location) {
+        console.log(location);
+
+        $('.location')
+            .append(location.city + ", ")
+            .append(location.region);
+
+        //var units = getUnits(location.country);
+        //getWeather(location.loc, units);
+
+        //return weather;
+
+    }, "jsonp");
+
 }
 
 
 $(document).ready(function () {
     'use strict';
 
-    $("#getlocation").on('click', function () {
-        getLocation();
-
-    });
+    getLocation();
 
 });
