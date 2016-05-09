791be01ba4c968fd51a52db113ec91f0a3f76204
sharpar <shivain27@gmail.com>
2016-05-09 09:02:27 -0400

    trying different api for location

--- myscript.js
+++ myscript.js
@@ -10,6 +10,111 @@ function toTitleCase(str) {
     });
 }
 
+/*var geocoder;
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+}
+//Get the latitude and the longitude;
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng)
+}
+
+function errorFunction() {
+    alert("Geocoder failed");
+}
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+}
+
+function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({
+        'latLng': latlng
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results)
+            if (results[1]) {
+                //formatted address
+                alert(results[0].formatted_address)
+                    //find country name
+                for (var i = 0; i < results[0].address_components.length; i++) {
+                    for (var b = 0; b < results[0].address_components[i].types.length; b++) {
+
+                        //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                        if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                            //this is the object you are looking for
+                            city = results[0].address_components[i];
+                            break;
+                        }
+                    }
+                }
+                //city data
+                alert(city.short_name + " " + city.long_name)
+
+
+            } else {
+                alert("No results found");
+            }
+        } else {
+            alert("Geocoder failed due to: " + status);
+        }
+    });
+}
+*/
+
+function displayLocation(latitude, longitude) {
+    var request = new XMLHttpRequest();
+
+    var method = 'GET';
+    var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+    var async = true;
+
+    request.open(method, url, async);
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var data = JSON.parse(request.responseText);
+            var address = data.results[0];
+            console.log(address.formatted_address);
+        }
+    };
+    request.send();
+};
+
+var successCallback = function (position) {
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    displayLocation(x, y);
+};
+
+var errorCallback = function (error) {
+    var errorMessage = 'Unknown error';
+    switch (error.code) {
+    case 1:
+        errorMessage = 'Permission denied';
+        break;
+    case 2:
+        errorMessage = 'Position unavailable';
+        break;
+    case 3:
+        errorMessage = 'Timeout';
+        break;
+    }
+    document.write(errorMessage);
+};
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 1000,
+    maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+
 function getLocation() {
     'use strict';
 
@@ -49,20 +154,12 @@ function getLocation() {
             temperature = parseFloat((temperature).toFixed(0));
             console.log(temperature);
 
-            if (units === "imperial") {
-                unitLabel = "F";
-            } else {
-                unitLabel = "C";
-            }
-
-            //temperature = parseFloat((temperature).toFixed(1));
-
             console.log(weather);
 
             $('.location').append(weather.name + ", " + weather.sys.country);
             $('#conditions').append(toTitleCase(weather.weather[0].description));
             $('#humidity').append("Humidity: " + weather.main.humidity + "%");
-            $('#wind').append(weather.wind.speed);
+            $('#wind').append(windDir + " " + weather.wind.speed + " knots");
 
         }, "jsonp");
 
@@ -83,6 +180,7 @@ function getLocation() {
 
 $(document).ready(function () {
     'use strict';
+    //initialize();
     getLocation();
 
 });
