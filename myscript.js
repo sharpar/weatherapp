9ef0f5a8b5cdaacedaecb9265066584ba6c15592
sharpar <shivain27@gmail.com>
2016-05-12 13:33:38 +0400

    cleaning up code

--- myscript.js
+++ myscript.js
@@ -6,14 +6,6 @@
 
 var unit = "si";
 
-function toTitleCase(str) {
-    'use strict';
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-
-}
-
 function removeDec(num) {
     'use strict';
     return parseFloat((num).toFixed(0));
@@ -23,41 +15,25 @@ function getLocation(unit) {
     'use strict';
 
     if (!navigator.geolocation) {
-        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        $('#icon').html("<p>Geolocation is not supported by your browser</p>");
         return;
-    }
-
-    function convertWindDirection(dir) {
-        var rose = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'],
-            eightPoint = Math.floor(dir / 45);
-        return rose[eightPoint];
     }
 
     function success(position) {
         var latitude = position.coords.latitude,
             longitude = position.coords.longitude,
-            img = new Image(),
-            appid = "&APPID=c0e613d7638b0b5e8fc7d54e1d673a86",
+            //img = new Image(),
             forecast = "http://crossorigin.me/https://api.forecast.io/forecast/de58b48418b7a1930004d32486ff7c93/" + (latitude).toString() + "," + (longitude).toString() + "?units=" + unit;
-        console.log(unit);
+
         //img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=400x200&sensor=false";
 
-        //output.appendChild(img);
-
         $.getJSON(forecast, function (data) {
-            console.log(data);
-            console.log(data.timezone);
 
             var city = data.timezone.substr(data.timezone.indexOf("/") + 1).replace("-", " "),
                 temperature = data.currently.temperature,
                 feels = data.currently.apparentTemperature,
-                conditions = toTitleCase(data.currently.summary),
                 icon = $('#icon'),
                 dataicon = data.daily.icon;
-
-
-            console.log(icon.attr("data-icon"));
-
 
             $('h1').html(city);
 
@@ -111,31 +87,24 @@ function getLocation(unit) {
                 icon.attr("data-icon", "");
             }
 
-            console.log(icon.attr("data-icon"));
-
-            //if (conditions === "Clear")
-
             $('#minmax').html(removeDec(data.daily.data[0].temperatureMax) + "\xB0 | " + removeDec(data.daily.data[0].temperatureMin) + "\xB0");
-            if (unit == "si") {
+            if (unit === "si") {
                 $('#temp').html(removeDec(temperature) + "\xB0 C" + '<span id="convert"> F</span>');
-                $('#feelslike').html("Feels like " + removeDec(feels) + "\xB0 C");
             } else {
                 $('#temp').html(removeDec(temperature) + "\xB0 F" + '<span id="convert"> C</span>');
-                $('#feelslike').html("Feels like " + removeDec(feels) + "\xB0 F");
             }
-            $('#conditions').html(conditions);
-            $('#humidity').html("Humidity - " + (data.currently.humidity * 100) + "%");
+            $('#feelslike').html("Feels like " + removeDec(feels) + "\xB0");
+            $('#conditions').html(data.currently.summary.replace('-', ' '));
+            $('#humidity').html("Humidity: " + (data.currently.humidity * 100) + "%");
 
         });
 
     }
 
     function error() {
-        output.innerHTML = "Unable to retrieve your location";
+        $('#icon').html("Unable to retrieve your location");
     }
-
     navigator.geolocation.getCurrentPosition(success, error);
-
 }
 
 
@@ -152,7 +121,6 @@ $(document).ready(function () {
             unit = "si";
             getLocation("si");
         }
-
     });
 
 });
